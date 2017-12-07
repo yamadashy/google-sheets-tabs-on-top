c6d669286a78bc4419fd88ed62c62618681f248d
山田和毅 <yamada.kazuki.1198@colopl.co.jp>
2017-12-08 01:14:22 +0900

    オブザーバー使う

--- js/content.js
+++ js/content.js
@@ -1,6 +1,21 @@
-(() => {
+function moveTabsToTop() {
     const gridBottomBar = document.getElementById("grid-bottom-bar");
     const editorContainer = document.getElementById("docs-editor-container");
     gridBottomBar.style.zIndex = 0;
     document.body.insertBefore(gridBottomBar, editorContainer);
-})();
+}
+
+function setObserver(parent, target, callback) {
+    const observer = new MutationObserver((mutationRecords, observer) => {
+        if (document.getElementById(target)) {
+            observer.disconnect();
+            callback();
+        }
+    });
+    observer.observe(document.getElementById(parent), {
+        childList:true,
+        subtree: true
+    });
+}
+
+setObserver("docs-editor-container", "grid-bottom-bar", moveTabsToTop);
