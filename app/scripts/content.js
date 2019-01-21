6651a752125327041259a0d444624265df7651c9
Kazuki Yamada <kaz-yamada@colopl.co.jp>
2019-01-22 02:17:18 +0900

    fix indent

--- app/scripts/content.js
+++ app/scripts/content.js
@@ -1,6 +1,6 @@
 (() => {
-    const gridBottomBar = document.getElementById("grid-bottom-bar");
-    const editorContainer = document.getElementById("docs-editor-container");
-    gridBottomBar.style.zIndex = 0;
-    document.body.insertBefore(gridBottomBar, editorContainer);
+  const gridBottomBar = document.getElementById("grid-bottom-bar");
+  const editorContainer = document.getElementById("docs-editor-container");
+  gridBottomBar.style.zIndex = 0;
+  document.body.insertBefore(gridBottomBar, editorContainer);
 })();
