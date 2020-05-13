ca247cf60eb5f1f1a03fb74ae7b9e958431bd3bd
Kazuki Yamada <yamadashy@yamaMac.local>
2020-05-14 02:34:46 +0900

    update: auto detect when install

---
+++ app/scripts/background.ts
@@ -0,0 +1,18 @@
+// Update extension content for slack tabs
+chrome.tabs.query({}, (tabs) => {
+  for (const tabKey in tabs) {
+    const tab = tabs[tabKey];
+
+    // Do not have permission
+    if (tab.url === undefined) {
+      continue;
+    }
+
+    chrome.tabs.executeScript(tab.id, {
+      file: 'scripts/content.js',
+      runAt: 'document_idle',
+      allFrames: true
+    });
+  }
+});
+
